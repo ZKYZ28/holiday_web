101c4443bde21f48e908590bd0540c7c1e662f07
Edwin De Vlegelaer <e.devlegelaer@student.helmo.be>
2023-09-28 20:58:36 +0200

    Contact view

--- src/components/common/FormInput.tsx
+++ src/components/common/FormInput.tsx
@@ -1,28 +1,31 @@
-import {useState} from "react";
+import {ChangeEvent, useState} from "react";
 import Span from "./Span.tsx";
+import StarsRequired from "./StarsRequired.tsx";
 
 function FormInput({styleInput, idInput, nameInput, errorMessage, label, onChange, ...inputProps}) {
 
     const [focused, setFocused] = useState(false);
+    const [isValid, setIsValid] = useState(true);
 
-    const handleFocus = (e) => {
-        setFocused(true);
+    const handleBlur = (e:ChangeEvent<HTMLInputElement>) => {
+        setFocused(false);
+        setIsValid(e.currentTarget.checkValidity());
     };
 
     return (
         <div className="formInput">
-            <label htmlFor={idInput}>{label}</label>
+            <label htmlFor={idInput}>{label}<StarsRequired /></label>
             <input
                 {...inputProps}
-                className={styleInput}
+                className={`${styleInput} ${!isValid && focused ? 'border-red-500' : ''}`}
                 id={idInput}
                 name={nameInput}
                 onChange={onChange}
-                onBlur={handleFocus}
+                onBlur={handleBlur}
                 onFocus={() => setFocused(true)}
-                focused={focused.toString()}
+                data-focused={focused.toString()}
             />
-            <Span style="text-red-600" text={errorMessage}></Span>
+            {!isValid && <Span style="text-red-600" text={errorMessage}></Span>}
         </div>
     )
 }
--- src/components/common/TextAreaInput.tsx
+++ src/components/common/TextAreaInput.tsx
@@ -1,27 +1,33 @@
-import {useState} from "react";
+import {ChangeEvent, useState} from "react";
 import Span from "./Span.tsx";
+import StarsRequired from "./StarsRequired.tsx";
 
-function TextAreaInput({styleTextArea, idTextArea, nameTextArea, errorMessage, textLabel, onChange, ...inputProps}) {
+function TextAreaInput({styleTextArea, idTextArea, nameTextArea, errorMessage, label, onChange, ...inputProps}) {
+    console.log(inputProps)
 
     const [focused, setFocused] = useState(false);
+    const [isValid, setIsValid] = useState(true);
 
-    const handleFocus = (e) => {
-        setFocused(true);
+    const handleBlur = (e:FocusEvent<HTMLTextAreaElement>) => {
+        setFocused(false);
+        // Fais la validation, comme ceci car il n'y a pas de pattern en HTML standard pour les balises <text area>
+        setIsValid(e.currentTarget.value.length >= 10);
     };
 
     return (
         <div className="textArea">
-            <label htmlFor={idTextArea}>{textLabel}</label>
+            <label htmlFor={idTextArea}>{label}<StarsRequired /></label>
             <textarea
                 {...inputProps}
-                className={styleTextArea}
+                className={`${styleTextArea} ${!isValid && focused ? 'border-red-500' : ''}`}
                 id={idTextArea}
                 name={nameTextArea}
                 onChange={onChange}
+                onBlur={handleBlur}
                 onFocus={() => setFocused(true)}
-                focused={focused.toString()}
+                data-focused={focused.toString()}
             />
-            <Span style="text-red-600" text={errorMessage}></Span>
+            {!isValid && <Span style="text-red-600" text={errorMessage}></Span>}
         </div>
     )
 }
