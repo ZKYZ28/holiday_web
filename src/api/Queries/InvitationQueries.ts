4b5235eeb70d66e25632c47b926009b93f85c146
q210208 <f.mahy@student.helmo.be>
2023-10-15 10:24:10 +0200

    Add Invitations Fix

--- src/api/Queries/InvitationQueries.ts
+++ src/api/Queries/InvitationQueries.ts
@@ -3,10 +3,10 @@ import HolidayApi from "../EndPoints/HolidayApi.ts";
 import {invitationsKeys} from "../Querykeys.ts";
 import {InvitationMutation} from "../Models/Invitation.ts";
 
-export const useGetInvitations = (participanId : string) => {
+export const useGetInvitations = (participantId : string) => {
   return useQuery({
     queryKey: ['invitation'],
-    queryFn: () => HolidayApi.getInvitations(participanId).then((content) => content.data),
+    queryFn: () => HolidayApi.getInvitations(participantId).then((content) => content.data),
     initialData: [],
   });
 };
